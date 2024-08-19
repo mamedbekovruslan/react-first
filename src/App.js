ba5415b8e51afddc95a5b92ebe48428003ea9ab8
RMamedbekov <mamedbekovruslan@yandex.ru>
2024-08-19 22:06:02 +0300

    Re-write code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  // Весь код кроме помеченной строки - декларатиный
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* Вот это императиный стиль. */}
-        <span>{new Date().getFullYear()}</span>
-      </header>
-    </div>
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement(
+      "header",
+      { className: "App-header" },
+      React.createElement("img", {
+        src: logo,
+        className: "App-logo",
+        alt: "logo",
+      }),
+      React.createElement(
+        "p",
+        null,
+        "Edit ",
+        React.createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      React.createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      ),
+      React.createElement("span", null, new Date().getFullYear())
+    )
   );
 }
 
